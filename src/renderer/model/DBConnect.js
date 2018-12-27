4dfe33cc34b3654f7f6c097aff03ee1a535b5873
STONE <1037311800@qq.com>
2018-12-28 02:22:51 +1100

    more details for DoB records operations

--- src/renderer/model/DBConnect.js
+++ src/renderer/model/DBConnect.js
@@ -1,10 +1,10 @@
 var Datastor = require('nedb');
-var db = new Datastor({filename: '/file/DoB.db', autoload: true});
+var db = new Datastor({filename: '/file/DoB.json', autoload: true});
 
 const dbAccess = {
     findAll: function () {
         db.find({})
-            .sort({dob: 1})
+            .sort({"DoB": 1}) // 1 升序， -1 降序
             .exec(function (err, docs) {
                 if (!err) {
                     console.log(docs);
@@ -16,35 +16,66 @@ const dbAccess = {
 
     findOneWeek: function () {
         // 按日期排序，取出最近七日的
-        db.find({})
-            .sort({dob: 1})
-            .exec(function (err, docs) {
-                if (!err) {
-                    console.log(docs);
-                } else {
-                    console.log(err);
-                }
-            });
+        // db.find({})
+        //     .sort({dob: 1})
+        //     .exec(function (err, docs) {
+        //         if (!err) {
+        //             console.log(docs);
+        //         } else {
+        //             console.log(err);
+        //         }
+        //     });
     },
 
-    insertOne: function () {
-        // db.
+    insertOne: function (doc) {
+        db.insert(doc, function (err, newDoc) {
+            if (newDoc != null) {
+                console.log(newDoc);
+            } else {
+                console.log(err);
+            }
+        });
     },
 
     importRecords: function () {
-
+        // 先读文件，转为JSON形式
+        // 然后db.insert([{}, {}], function (err, newDoc) {})
     },
 
-    updateOne: function () {
-
+    updateOne: function (doc) {
+        // 在输入端控制用户输入
+        db.update({"_id": doc._id}, {$set: {"DoB": doc.DoB, "category": doc.category}}, {}, function (err, numReplaced) {
+            if (err != null) {
+                console.log(err);
+            } else {
+                console.log(numReplaced);
+            }
+        })
     },
 
-    deleteOne: function () {
+    deleteOne: function (doc) {
+        db.remove({"_id": doc._id}, {}, function (err, numReplaced) {
+            if (err != null) {
+                console.log(err);
+            } else {
+                console.log(numReplaced);
+            }
+        });
+    },
 
+    deleteAll: function () {
+        db.remove({}, {multi: true}, function (err, numReplaced) {
+            if (err != null) {
+                console.log(err);
+            } else {
+                console.log(numReplaced);
+            }
+        })
     },
 
     exportRecords: function () {
-
+        // 选定输出位置，并创建KeepinTouch_date.json文件
+        // 输出所有记录，保存关闭
     }
 };
 
